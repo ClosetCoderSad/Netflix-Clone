821687e9843fed0a4b4bf38c3c3ef858067eb635
ClosetCoderSad <abrarroll2@gmail.com>
2024-09-14 18:28:57 -0500

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -34,7 +35,7 @@ const signup = async (name, email, password)=>{
         });
     } catch (error) {
         console.log(error);
-        alert(error);   
+        toast.error(error.code.split('/')[1].split('-').join(" "));   
     }
 }
 
@@ -43,7 +44,7 @@ const login = async (email, password)=>{
         await signInWithEmailAndPassword(auth, email, password);
     } catch (error) {
         console.log(error);
-        alert(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
     }
 }
 const logout = ()=>{
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,23 +2,31 @@ import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
 import { login, signup } from '../../firebase'
+import netflix_spinner from '../../assets/netflix_spinner.gif'
 
 const Login = () => {
   const [signState, setSignState] = useState("Sign In")
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
 
   const user_auth = async (event)=>{
     event.preventDefault();
+    setLoading(true);
     if(signState==="Sign In"){
       await login(email, password);
     }else{
       await signup(name, email, password);
     }
+    setLoading(false);
   }
 
   return (
+    loading? <div className='login-spinner'>
+      <img src={netflix_spinner} alt=''/>
+    </div>:
     <div className='login'>
       <img src={logo} className='login-logo' alt=''/>
       <div className='login-form'>
